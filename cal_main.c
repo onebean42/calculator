3f4916391654ef28dabd33a60a89d3793f470b2c
onebean42 <102871129+onebean42@users.noreply.github.com>
2022-11-25 17:12:29 +0900

    Update cal_main.c

--- cal_main.c
+++ cal_main.c
@@ -4,17 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    int b = atoi(argv[1]);
+    int c = atoi(argv[2]);
 
-    printf("<===============>\n");
-    printf("<===============>\n");
-    printf("<===============>\n");
-    printf("<===============>\n");
-    printf("%d + %d => %d\n", a, b, add(a, b));
-    printf("%d - %d => %d\n", a, b, sub(a, b));
-    printf("%d * %d => %d\n", a, b, mul(a, b));
-    printf("%d / %d => %d\n", a, b, dur(a, b));
+    printf("===============>\n");
+    printf("===============>\n");
+    printf("===============>\n");
+    printf("===============>\n");
+    printf("%d + %d => %d\n", b, c, add(b, c));
+    printf("%d - %d => %d\n", b, c, sub(b, c));
+    printf("%d * %d => %d\n", b, c, mul(b, c));
+    printf("%d / %d => %d\n", b, c, dur(b, c));
     printf("===============\n");
 
     return 0;
